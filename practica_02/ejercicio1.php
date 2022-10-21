c807f34e7849392ac4fa80f4bcda2d2addf3b867
josemanuelponce <josee.manuel.9700@gmail.com>
2022-10-21 09:36:16 +0200

    commit inicial

--- practica_02/ejercicio1.php
+++
@@ -1,9 +0,0 @@
-<form action="ejercicio1.php" method="get">
-<label>Numero 1</label><br>
-<input type="text" name="a"><br><br>
-<label>Numero 2</label><br>
-<input type="text" name="b"><br><br>
-<input type="hidden" name="f" value="numero">
-<input type="submit" value="Enviar">
-</form>
-
