39692a0b7d1d4ac2ee6feedf1d8b68cef9d500fc
josemanuelponce <josee.manuel.9700@gmail.com>
2022-10-25 11:14:45 +0200

    commit inicial

---
+++ 04_arrays/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arrays</title>
+</head>
+<body>
+    <?php
+    //Videojuego
+    $videojuegos = array(
+        "juego1" => "Cyberpunk 2077",
+        "juego2" => "Minecraft",
+        "juego3" => "My little piny"
+    );
+    print_r($videojuegos);
+    //Consolas
+    $consolas = [
+        "consola1" => "PS4",
+        "consola2" => "PS5",
+        "consola3" => "PS3"
+    ];
+    print_r($consolas);
+    
+    
+    ?>
+
+
+
+
+
+
+
+
+</body>
+</html>
