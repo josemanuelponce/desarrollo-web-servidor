fac6b214dbb3835a4077062c2e86ce2b5d1d5da1
josemanuelponce <josee.manuel.9700@gmail.com>
2022-11-11 10:46:18 +0100

    compras total

--- 06_base_de_datos/tienda_ropa/public/compras/mostrar_compras.php
+++ 06_base_de_datos/tienda_ropa/public/compras/mostrar_compras.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,33 +8,66 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <title>Compras</title>
 </head>
+
 <body>
+    <?php require '../../util/base_de_datos.php' ?>
+    <?php require '../header.php' ?>
+    <br>
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "GET") {
         $usuario = $_GET["usuario"];
     }
     ?>
-    <div class="row">
-            <div class="col-4">
-                <p>Nombre: <?php echo $nombre ?></p>
-                <p>Talla: <?php echo $talla ?></p>
-                <p>Precio: <?php echo $precio ?></p>
-                <p>Categoría: <?php echo $categoria ?></p>
-                <a class="btn btn-secondary" href="index.php">Volver</a>
-                <form action="editar_prenda.php" method="get">
-                    <input type="hidden" name="id" value="<?php echo $id ?>">
-                    <input type="hidden" name="nombre" value="<?php echo $nombre ?>">
-                    <input type="hidden" name="talla" value="<?php echo $talla ?>">
-                    <input type="hidden" name="precio" value="<?php echo $precio ?>">
-                    <input type="hidden" name="categoria" value="<?php echo $categoria ?>">
-                    <button type="submit" class="btn btn-primary">Editar</button>
-                </form>
-            </div>
-            <div class="col-4">
-                <img witdh="200" height="300" src="../..<?php echo $imagen ?>">
-            </div>
-    <h1>Compras de <?php echo $usuario ?></h1>
+    <div class="container">
+        <h1>Compras de <?php echo $usuario ?></h1>
+        <div class="row">
+            <div class="col-9">
+                <table class="table">
+                    <thead class="table table-dark">
+                        <tr>
+                            <th>producto</th>
+                            <th>Cantidad</th>
+                            <th>Precio</th>
+                            <th>Subtotal</th>
+                            <th>Fecha</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php 
+                        $sql = "SELECT * FROM vw_clientes_prendas
+                            WHERE usuario = '$usuario'";
+                            $resultado = $conexion->query($sql);
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+                            $precio_total = 0;
+                            if ($resultado->num_rows > 0) {
+                                while ($fila = $resultado->fetch_assoc()) {
+                                    $producto = $fila["producto"];
+                                    $cantidad = $fila["cantidad"];
+                                    $precio_unitario = $fila["precio_unitario"];
+                                    $fecha = $fila["fecha"];
+                                    $precio_total += ($precio_unitario * $cantidad);
+                                    ?>
+                                        <tr>
+                                            <td><?php echo $producto ?></td>
+                                            <td><?php echo $cantidad ?></td>
+                                            <td><?php echo $precio_unitario ?></td>
+                                            <td><?php echo $precio_unitario * $cantidad ?></td>
+                                            <td><?php echo $fecha ?></td>
+                                        </tr>
+                                    <?php
+
+                                }
+                            }
+                        ?>
+                    </tbody>
+                </table>
+                <h4><span class="badge bg-success">Total: <?php echo $precio_total ?>€</span></h4>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
+
 </html>
