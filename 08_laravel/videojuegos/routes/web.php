1dbd84a5e99704e9fb4429b1b71a51e13522c8a8
josemanuelponce <josee.manuel.9700@gmail.com>
2022-11-22 12:19:28 +0100

    commit inicial

---
+++ 08_laravel/videojuegos/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\VideojuegosController;
+use App\Http\Controllers\ConsolasController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/consolas/info', function() {
+    return view('consolas/info');
+});
+
+//Consolas
+Route::get('/consolas', [ConsolasController::class, 'index']);
+
+Route::get('/consolas/create', [ConsolasController::class, 'create']);
+
+//Videojuegos
+Route::get('/videojuegos', [VideojuegosController::class, 'index']);
+
+Route::get('/videojuegos/create', [VideojuegosController::class, 'create']);
+
