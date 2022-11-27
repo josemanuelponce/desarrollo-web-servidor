890a13df0e58cb571bb431162ea676d96d7a6578
josemanuelponce <josee.manuel.9700@gmail.com>
2022-11-27 16:04:45 +0100

    commit footer

--- 06_base_de_datos/tienda_ropa/public/index.php
+++ 06_base_de_datos/tienda_ropa/public/index.php
@@ -45,9 +45,12 @@
           <span class="visually-hidden">Next</span>
         </button>
       </div>
+      
     </div>
     <div class="caja_index">
     </div>
+    <br><br><br>
+      <?php require 'footer.php' ?>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
